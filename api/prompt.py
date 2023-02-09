62ca04dd2dbf75cbde0f4da66c7a7fdc9fb79ded
ytl0623 <david89062388@gmail.com>
2023-02-09 18:09:59 +0800

    Update prompt.py

--- api/prompt.py
+++ api/prompt.py
@@ -13,12 +13,7 @@ class Prompt:
         self.msg_list.append(f"AI:{LANGUAGE_TABLE[chat_language]}")
     
     def add_msg(self, new_msg):
-        if len(self.msg_list) >= MSG_LIST_LIMIT:
-            self.remove_msg()
         self.msg_list.append(new_msg)
-
-    def remove_msg(self):
-        self.msg_list.pop(0)
 
     def generate_prompt(self):
         return '\n'.join(self.msg_list)
