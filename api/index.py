450817760a68eca2871be5f54952ee4ae767a7e1
ytl0623 <david89062388@gmail.com>
2023-03-10 18:22:11 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -68,7 +68,7 @@ def handle_message(event):
         
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=reply_msg))
+            TextSendMessage(text=type(reply_msg)))
 
 
 if __name__ == "__main__":
