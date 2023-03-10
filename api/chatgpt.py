6062f57fc93d59787fa57c82fc53674fac58b094
ytl0623 <david89062388@gmail.com>
2023-03-10 17:52:24 +0800

    Update chatgpt.py

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -19,10 +19,10 @@ class ChatGPT:
         # Find the first response from the chatbot that has text in it (some responses may not have text)
         for choice in response.choices:
             if "text" in choice:
-                return "123"  #choice.text
+                return choice.text
 
         # If no response with text is found, return the first response's content (which may be empty)
-        return "456"  #response.choices[0].message.content
+        return response.choices[0].message.content
     
     
     
