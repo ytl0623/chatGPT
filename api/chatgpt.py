354f80e5aacf5ee4c5b0c0e369bc1c555d4d0895
ytl0623 <david89062388@gmail.com>
2023-03-10 18:35:08 +0800

    Update chatgpt.py

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -7,7 +7,6 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 class ChatGPT:
     def get_response(user_input):
-        return "123"
         # Use OpenAI's ChatCompletion API to get the chatbot's response
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",  # The name of the OpenAI chatbot model to use
@@ -16,7 +15,7 @@ class ChatGPT:
             stop=None,              # The stopping sequence for the generated response, if any (not used here)
             temperature=0.7,        # The "creativity" of the generated response (higher temperature = more creative)
         )
-        return "123"
+        
         # Find the first response from the chatbot that has text in it (some responses may not have text)
         for choice in response.choices:
             if "text" in choice:
