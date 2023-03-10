8ee5d48db8d8cc00465922d166e2e87fff4a330e
ytl0623 <david89062388@gmail.com>
2023-03-10 19:41:24 +0800

    Update chatgpt.py

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -7,12 +7,13 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 class ChatGPT:
     def __init__(self):
+        self.message = ""
         
-    def get_response( self, user_input ):
+    def get_response(self):
         # Use OpenAI's ChatCompletion API to get the chatbot's response
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",  # The name of the OpenAI chatbot model to use
-            messages=[ {"role": "system", "content": user_input} ],   # The conversation history up to this point, as a list of dictionaries
+            messages=[ {"role": "system", "content": self.message} ],   # The conversation history up to this point, as a list of dictionaries
             max_tokens=3800,        # The maximum number of tokens (words or subwords) in the generated response
             stop=None,              # The stopping sequence for the generated response, if any (not used here)
             temperature=0.7,        # The "creativity" of the generated response (higher temperature = more creative)
@@ -28,4 +29,5 @@ class ChatGPT:
     
     
     
-    
+    def add_msg(self, text):
+        self.message = text
